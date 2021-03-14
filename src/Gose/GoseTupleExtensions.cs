510535794aa33482f3a0d03f9b3553c4a2f45a99
Garrett van Wageningen <ghvanw283@gmail.com>
2021-03-13 19:29:54 -0600

    initish commit

---
+++ src/Gose/GoseTupleExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gose {
+
+    public static class GoseTupleExtensions {
+
+        public static (Err? Error, Result? Data) Select<Err, Data, Result>(
+            this (Err? Error, Data? Data) @this, 
+            Func<Data, Result> fn) =>
+            @this switch {
+                (null, Data d) => (default, fn(d)),
+                _ => (@this.Error, default)
+            };
+
+
+        public static (Err? Error, Result? Data) SelectMany<Err, Data, Result>(
+            this (Err? Error, Data? Data) @this, 
+            Func<Data, (Err?, Result?)> fn) =>
+            @this switch {
+                (null, Data d) => fn(d),
+                _ => (@this.Error, default)
+            };
+
+
+        public static (Err? Error, Select? Data) SelectMany<Err, Data, Result, Select>(
+            this (Err? Error, Data? Data) @this,
+            Func<Data, (Err?, Result?)> fn,
+            Func<Data, Result, Select> selector) =>
+            @this switch {
+                (null, Data d) => fn(d).Select(data => selector(d, data)),
+                _ => (@this.Error, default)
+            };
+
+        
+        public static (Result? Error, Data? Data) SelectErr<Err, Data, Result>(
+            this (Err? Error, Data? Data) @this,
+            Func<Err, Result> fn) =>
+            @this switch {
+                (Err e, null) => (fn(e), default),
+                _ => (default, @this.Data)
+            };
+    }
+}
---
+++ tests/Gose.Test/GoseTupleExtensionsTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Gose.Test {
+
+    public class GoseTupleExtensionsTests {
+
+        [Fact]
+        public void SecondFail() {
+            (string? Error, int? Data) first = (null, 10);
+            (string? Error, int? Data) second = ("divide by zero", null);
+
+            var (error, data) =
+                (from x in first
+                 from y in second
+                 select x + y);
+
+            Assert.Equal("divide by zero", error);
+            Assert.Null(data);
+        }
+
+        [Fact]
+        public void FirstFail() {
+            (string? Error, int? Data) first = ("no data available", null);
+            (string? Error, int? Data) second = (null, 20);
+
+            var (error, data) =
+                (from x in first
+                 from y in second
+                 select x + y);
+
+            Assert.Equal("no data available", error);
+            Assert.Null(data);
+        }
+
+        [Fact]
+        public void Success() {
+            (string? Error, int? Data) first = (null, 10);
+            (string? Error, int? Data) second = (null, 20);
+
+            var (error, data) =
+                (from x in first
+                 from y in second
+                 select x + y);
+
+            Assert.Equal(30, data);
+            Assert.Null(error);
+        }
+
+        [Fact]
+        public void SelectErr() {
+            (int? Error, int? Data) result = (5, null);
+
+            var (error, data) = result.SelectErr(err => $"Error #{err} returned");
+
+            Assert.Equal("Error #5 returned", error);
+            Assert.Null(data);
+        }
+    }
+}
